e8d7c552e1b3c7c925aa9134464dd0a49cb579c3
kmpizmad <nagy.viktor392@gmail.com>
2021-03-09 21:39:15 +0100

    fix build

--- utils/classes/Stream/index.ts
+++ utils/classes/Stream/index.ts
@@ -1,5 +1,4 @@
 import { StreamBase } from './StreamBase';
 import { StreamReader } from './StreamReader';
-import { StreamWriter } from './StreamWriter';
 
-export { StreamBase, StreamReader, StreamWriter };
+export { StreamBase, StreamReader };
